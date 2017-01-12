d6d8192f8587b7aa15332a214bc65404b4af646f
Filip Bartoš <filip@bartos.me>
2017-01-12 17:34:15 +0100

    Updated index.js to support more font types at once

--- index.js
+++ index.js
@@ -10,47 +10,83 @@ var template = [
   '  font-family: "{{fontName}}";\n',
   '  font-style: {{fontStyle}};\n',
   '  font-weight: {{fontWeight}};\n',
-  '  src: url("data:application/x-font-{{fontType}};base64,{{base64}}") format("{{fontType}}");\n',
+  '  src: url("data:{{fontMime}};base64,{{base64}}") format("{{fontFormat}}");\n',
   '}\n'
 ].join('')
+var meta = {
+  ttf: {
+    mimetype: 'application/font-sfnt',
+    format: 'truetype'
+  },
+  woff: {
+    mimetype: 'application/font-woff',
+    format: 'woff'
+  },
+  woff2: {
+    mimetype: 'font/woff2',
+    format: 'woff2'
+  },
+}
 var defaults = {
-  filename: 'fonts.css'
+  filename: 'fonts',
+}
+function getFileExtension (filename) {
+  return path.extname(filename).slice(1, path.extname(filename).length)
 }
 function getFileInfo (file) {
   var filename = path.basename(file.relative)
-  var extension = path.extname(filename).slice(1, path.extname(filename).length)
+  var extension = getFileExtension(filename)
   var font = filename.slice(0, filename.length - (extension.length + 1))
   var props = font.split('-')
   return {
-    fontType: extension,
+    fontFormat: meta[extension]['format'],
+    fontMime: meta[extension]['mimetype'],
     fontName: props[0],
     fontWeight: props[1] || 400,
     fontStyle: props[2] || 'normal',
     base64: file.contents.toString('base64')
   }
 }
+function getFontTypes (fonts) {
+  var fontTypes = []
+  fonts.forEach(function (font) {
+    var filename = path.basename(font.relative)
+    var extension = getFileExtension(filename)
+    if(fontTypes.indexOf(extension) === -1) {
+      fontTypes.push(extension)
+    }
+  })
+  return fontTypes
+}
 module.exports = function (options) {
   var settings = Object.assign( {}, defaults, options );
-  var files = []
+  var fonts = []
   return through.obj(function(file, enc, callback){
-    files.push(file);
+    fonts.push(file);
     callback();
   }, function(callback) {
-    var vinyl = new Vinyl()
-    var buffer = ''
-    files.forEach(function (file) {
-      var fileInfo = getFileInfo(file)
-      buffer += template
-        .replace(/{{fontName}}/g, fileInfo.fontName)
-        .replace(/{{fontWeight}}/g, fileInfo.fontWeight)
-        .replace(/{{fontType}}/g, fileInfo.fontType)
-        .replace(/{{fontStyle}}/g, fileInfo.fontStyle.toLowerCase())
-        .replace(/{{base64}}/g, fileInfo.base64)
-    })
-    vinyl.path = path.resolve(vinyl.base, settings.filename)
-    vinyl.contents = new Buffer(buffer)
-    this.push(vinyl)
-    gutil.log(CODENAME, ': finished ( created ' + vinyl.path.replace(process.cwd(),'') + ')')
+    var fontTypes = getFontTypes(fonts)
+    fontTypes.forEach((function (fontType) {
+      var vinyl = new Vinyl()
+      var buffer = ''
+      fonts.forEach(function (font) {
+        var filename = path.basename(font.relative)
+        if (fontType === getFileExtension(filename)) {
+          var fontInfo = getFileInfo(font, settings)
+          buffer += template
+            .replace(/{{fontName}}/g, fontInfo.fontName)
+            .replace(/{{fontMime}}/g, fontInfo.fontMime)
+            .replace(/{{fontFormat}}/g, fontInfo.fontFormat)
+            .replace(/{{fontWeight}}/g, fontInfo.fontWeight)
+            .replace(/{{fontStyle}}/g, fontInfo.fontStyle.toLowerCase())
+            .replace(/{{base64}}/g, fontInfo.base64)
+        }
+      })
+      vinyl.path = path.resolve(vinyl.base, settings.filename+'-'+fontType+'.css')
+      vinyl.contents = new Buffer(buffer)
+      this.push(vinyl)
+      gutil.log(CODENAME, ': finished ( created ' + vinyl.relative + ')')
+    }).bind(this))
     callback()
   })
 };
