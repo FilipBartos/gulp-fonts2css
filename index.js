00b8a3e1e2e6b3c8afa215c346fa331f0e2202e7
Filip Bartoš <filip@bartos.me>
2017-01-12 12:43:46 +0100

    gulp-fonts2css commit

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+var CODENAME = 'gulp-fonts2css'
+var gutil = require('gulp-util')
+var through = require('through2')
+var path = require('path')
+var fs = require('fs')
+var Vinyl = require('vinyl')
+var template = [
+  '@font-face {\n',
+  '  font-family: "{{fontName}}";\n',
+  '  font-style: {{fontStyle}};\n',
+  '  font-weight: {{fontWeight}};\n',
+  '  src: url("data:application/x-font-{{fontType}};base64,{{base64}}") format("{{fontType}}");\n',
+  '}\n'
+].join('')
+var defaults = {
+  filename: 'fonts.css'
+}
+
+function getFileInfo (file) {
+  var filename = path.basename(file.relative)
+  var extension = path.extname(filename).slice(1, path.extname(filename).length)
+  var font = filename.slice(0, filename.length - (extension.length + 1))
+  return {
+    fontType: extension,
+    fontName: font.split('-')[0],
+    fontWeight: font.split('-')[1],
+    fontStyle: font.split('-')[2] ? font.split('-')[2] : 'normal',
+    base64: file.contents.toString('base64')
+  }
+}
+module.exports = function (options) {
+  var settings = Object.assign( {}, defaults, options );
+  var files = []
+  var outputFile = null
+  return through.obj(function(file, enc, callback){
+    outputFile = outputFile || file
+    files.push(file);
+    callback();
+  }, function(callback) {
+    var vinyl = new Vinyl()
+    var buffer = ''
+    files.forEach(function (file) {
+      var fileInfo = getFileInfo(file)
+      buffer += template
+        .replace('{{fontName}}', fileInfo.fontName)
+        .replace('{{fontWeight}}', fileInfo.fontWeight)
+        .replace('{{fontType}}', fileInfo.fontType)
+        .replace('{{fontStyle}}', fileInfo.fontStyle.toLowerCase())
+        .replace('{{base64}}', fileInfo.base64)
+    })
+    vinyl.path = path.resolve(vinyl.base, settings.filename)
+    vinyl.contents = new Buffer(buffer)
+    this.push(vinyl)
+    gutil.log(CODENAME, ': finished ( created ' + vinyl.path.replace(process.cwd(),'') + ')')
+    callback()
+  })
+};
